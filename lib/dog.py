a77ad9f1daa0ec929e5b243a85ac2cd1f1d0bcc7
Jospine <nzi19001@byui.edu>
2024-04-28 23:04:19 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -4,9 +4,14 @@ class Dog:
     def bark(self):
         print('woolf!')
         # Class body goes here
+    def talk(self):
+        print("Hello World!")
+        
     def sit(self):
         print("The dog is sitting.")
+    
 fido =Dog()
+fido.talk()
 fido.bark()
 fido.sit()
     #Instance method definition
